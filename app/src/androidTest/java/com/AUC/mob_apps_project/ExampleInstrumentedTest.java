7ae7e82b2aada32b0d8c3b8873f1f4a86e8f1145
Abdelsalam ElTamawy <solomspd@aucegypt.edu>
2020-01-12 18:52:48 +0200

    initial

--- app/src/androidTest/java/com/AUC/mob_apps_project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/AUC/mob_apps_project/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.AUC.mob_apps_project;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
