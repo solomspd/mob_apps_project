a54a34823dab3b2e8ef251d65b2eb0c95832f924
ramyelgendi <57822979+ramyelgendi@users.noreply.github.com>
2020-01-24 12:38:36 +0200

    About page done

--- app/src/main/java/com/AUC/mob_apps_project/splashscreen.java
+++ app/src/main/java/com/AUC/mob_apps_project/splashscreen.java
@@ -6,19 +6,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+import timber.log.Timber;
 
 
 public class splashscreen extends AppCompatActivity {
 //    private static int SPLASH_TIME_OUT = 10000;
 
     FirebaseAuth fAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +42,28 @@ public class splashscreen extends AppCompatActivity {
 //        },SPLASH_TIME_OUT);
 
          fAuth = FirebaseAuth.getInstance();
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Timber.w(task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+
+                        // Log and toast
+
+                        String msg = token;
+                        Timber.d(msg);
+
+                        FirebaseDatabase.getInstance().getReference("Tokens")
+                                .setValue(msg);
+                    }
+                });
 
         if(fAuth.getCurrentUser() != null){
             check();
